52b006ed29476badfdf00c4c50391c3265f5610b
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-07 16:23:08 +0600

    api all data setup

--- js/apps.js
+++ js/apps.js
@@ -1,13 +1,32 @@
 // API to dada fetch
-const loadPhone = async () =>{
+const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     const data = await res.json()
     const phones = data.data;
     displayPhones(phones);
 }
 // API data display
-const displayPhones = phones =>{
-    
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        const phoneCard = document.createElement('div');
+        phoneCard.classList.add('card', 'bg-base-100', 'w-96', 'shadow-xl');
+        phoneCard.innerHTML = `
+    <figure>
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+    `;
+        phoneContainer.appendChild(phoneCard);
+
+    })
 }
 
 
